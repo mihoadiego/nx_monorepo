0c362d198d416038607d6b0d7a24499a252e8c8e
mihoadiego <hoareaum@gmail.com>
2023-04-23 12:18:59 +0200

    update explanation about how to manage image and assets in the react app of the monorepo

--- apps/store/src/fake_api/index.ts
+++ apps/store/src/fake_api/index.ts
@@ -1,33 +1,39 @@
-
-import { GameProps, id } from "../app/typologies";
+import { GameProps, id } from '../app/typologies';
 
 const games: GameProps[] = [
-    {
-        id: 'pizza_party',
-        name: 'Pizza party',
-        image: "https://www.publicdomainpictures.net/pictures/450000/velka/woman-in-red-hat.jpg",
-        description: 'a dedicated game occuring at Piz Tower, with tomato and cheddar cheese of course!',
-        price: null,
-        mention: 'for youngs and olders'
-    },
-    {
-        id: 'spanish_run',
-        name: 'Spanish Run',
-        image: "https://www.publicdomainpictures.net/pictures/240000/velka/cow-1513926637hE2.jpg",
-        description: 'A spanish poor guy trying to understand the unfair world of Developers ',
-        mention: 'hardcore content, 18 years old and more',
-        rating: Math.floor(Math.random() * 11)
-    },
-    {
-        id: 'annecy_getaway',
-        name: 'Annecy getaway',
-        mention: '12 years old and +',
-        image: 'https://www.publicdomainpictures.net/pictures/150000/velka/colorful-test-tubes-14548653516ym.jpg',
-        description: 'A true GTA VICE CITY in the middle of the fortunate Annecians Assholes!',
-        rating: Math.floor(Math.random() * 11),
-        price: 150
-    }
-]
+  {
+    id: 'pizza_party',
+    name: 'Pizza party',
+    image:
+      'https://www.publicdomainpictures.net/pictures/450000/velka/woman-in-red-hat.jpg',
+    description:
+      'a dedicated game occuring at Piz Tower, with tomato and cheddar cheese of course!',
+    price: null,
+    mention: 'for youngs and olders',
+  },
+  {
+    id: 'spanish_run',
+    name: 'Spanish Run',
+    image:
+      'https://www.publicdomainpictures.net/pictures/240000/velka/cow-1513926637hE2.jpg',
+    description:
+      'A spanish poor guy trying to understand the unfair world of Developers ',
+    mention: 'hardcore content, 18 years old and more',
+    rating: Math.floor(Math.random() * 11),
+  },
+  {
+    id: 'annecy_getaway',
+    name: 'Annecy getaway',
+    mention: '12 years old and +',
+    // image: 'https://www.publicdomainpictures.net/pictures/150000/velka/colorful-test-tubes-14548653516ym.jpg',
+    image: '/assets/larson.jpg',
+    description:
+      'A true GTA VICE CITY in the middle of the fortunate Annecians Assholes!',
+    rating: Math.floor(Math.random() * 11),
+    price: 150,
+  },
+];
 
-export const getAllGames = ():GameProps[] => games;
-export const getGame =(id:id):GameProps|undefined => games.find(game => game.id === id);
+export const getAllGames = (): GameProps[] => games;
+export const getGame = (id: id): GameProps | undefined =>
+  games.find((game) => game.id === id);
--- explanations_step_by_step.ts
+++ explanations_step_by_step.ts
@@ -1,26 +1,31 @@
 /*
 
-source:
+===========================================================================================================
+source du tuto:
 https://egghead.io/lessons/javascript-install-and-use-external-npm-packages-in-an-nx-workspace
 
+===========================================================================================================
+toutes les commandes ci dessous, notamment les etapes 2.. 
+sont a faire en dehors d'un repos deja git cloné, 
+cest pour cela que je lai fait depuis home/workspace et non pas depuis un projet git deja fait (type node project)
 
 
-toutes les commandes ci dessous, notamment les etapes 2..; sont a faire en dehors d'un repos deja git cloné, cest pour cela que 
-je lai fait depuis home/workspace et non pas depuis un projet git deja fait (type node poroject)
-
-
-
-
-
-
-
-########################################################################################################
+===========================================================================================================
 toutes les commandes ou il va faire yarn nx dans le tuto, il faudra que je fasse npx nx a la place
 et quand il fait yarn add, c'est lequivalent de npm install 
-########################################################################################################
 
 
-1) cd workspace   (pour aller dans mon dossier workspace)
+
+
+
+===========================================================================================================
+===========================================================================================================
+PAS A PAS:
+===========================================================================================================
+===========================================================================================================
+
+
+1) cd workspace   (pour aller dans mon dossier local workspace)
 
 
 2) npx create-nx-workspace nxegghead
@@ -31,50 +36,50 @@ et quand il fait yarn add, c'est lequivalent de npm install
 
 
 
-
 3) cd nxegghead
 
-	et on va creer un fichier workspace.json, au meme niveau que le nx.json, qui contiendra ci dessous (fait sans que le tuto le dise, mais le tuto avait ce fichier dans son exemple)
-	{
-	    "version":1,
-	    "projects": {},
-	    "cli":{
-		"defaultCollection": "@nrwl/workspace"
-	    }
-	}
+	j'ai alors crée un fichier workspace_.json, au meme niveau que le nx.json, qui contient les éléments ci dessous 
+	(fait sans que le tuto le dise, mais le tuto avait ce fichier dans son exemple mais n'a pas été généré de mon coté)
+			{
+				"version":1,
+				"projects": {},
+				"cli":{
+				"defaultCollection": "@nrwl/workspace"
+				}
+			}
 
 
-########################################################################################################
-4) npx nx list     (pour avoir la liste de toutes les commandes disponibles dans nx a executer)
-########################################################################################################
+========= NOTA BENE
+  		 npx nx list     (pour avoir la liste de toutes les commandes disponibles dans nx a executer)
 
-5) installation des plugins nécessaires (workspace, react...)  (commandes lancées depuis le dossier nxegghead)
+
+
+
+
+4) installation des plugins nécessaires (workspace, react...)  (commandes lancées depuis la source du dossier nxegghead directement)
 	npm install @nrwl/nx-plugin -D     
 	npm install @nrwl/workspace -D     
 	npm install @nrwl/react
 
-########################################################################################################
-pour connaitre la liste des commandes/possibilités liées a un plungin (dans notre cas react), on lance la commande:
-	npx nx list @nrwl/react  
-########################################################################################################
+========= NOTA BENE
+		 npx nx list @nrwl/react   pour connaitre la liste des commandes/possibilités liées a un plugin (dans notre cas react):
 
 
-6 ) creation de notre application react: 
 
-	npx nx g @nrwl/react:application --help     (dabord pour connaitre toutes les options que l'on peut avoir pour npx nx g @nrwl/react:application)
-	
-	du coup on va faire 
-	
+
+5 ) creation de l'application react nommée 'store', via les commandes ci dessous: 
 	npx nx g @nrwl/react:application store   (pour creer une application react qui portera le nom de store)
-	
-	
-	
-	
-	
-	
-	################################################
-	il faudra peut etre rajouté, mais pas sure (a ce stade toujours pas eu besoin de le faire ), dans le fichier nx.json, les éléments ci dessous (VOIR SI CA NE FONCTIONNE PAS CAR AUTOMATIQUEMENT ILS NONT PAS ETE GENERE CHEZ MOI
-	QUAND J'AI LANCE MA COMMANDE, MAIS DANS CELUI DU TUTO OUI ALORS a RAJOUTEr A LA MAIN SI BESOIN... A SUIVRE
+
+========= NOTAB BENE
+	     npx nx g @nrwl/react:application --help     (commande pour lister les options dispo  pour npx nx g @nrwl/react:application)
+
+		
+========= NOTA BENE
+	il faudra peut etre rajouté, mais pas sure (a ce stade toujours pas eu besoin de le faire ), dans le fichier nx.json, 
+	les éléments ci dessous 
+	(voir si nécessaire, car les lignes ci dessous n'ont pas été générées sur mon poste quand j'ai exec les commandes du tuto, 
+	 pourtant, dans les fichiers du tuto, ces lignes ont été rajoutées automatiquement
+	 a voir si besoin de les rajouter a la main pour la suite, pour l'instant c'est pas bloquant, a suivre... 
 				  "implicitDependencies": { 
 				    "package.json": {
 				      "dependencies": "*",
@@ -94,23 +99,26 @@ pour connaitre la liste des commandes/possibilités liées a un plungin (dans no
 				    }
 				  },
 				  
-				  
-				  
-				  ET DANS tasksRunnerOptions.options aussi rajoutER 
+				  et dans  tasksRunnerOptions.options aussi rajoutER 
 				  "showUsageWarnings": true
-	ET DU COUP GRAND MISTER CONCERNANT LE workspace.json file, que je n'ai aps en local mais qui semble mis a jour et utilisé dans le tuto, a voir (
-	################################################
+	ET DU COUP GRAND MISTER CONCERNANT LE workspace_.json file, que jai crée a la main en local 
+	ce fichier workspace.json, (auquel moi j'ai rajouté un _ en local car visiblement pas généré) semble mis a jour et utilisé dans le tuto, 
+	a voir
 
-7) pour lancer mon app react qui se nomme store, toujours depuis le dossier nxegghead:
+
+
+
+6) pour lancer mon app react qui se nomme store, toujours depuis le dossier nxegghead:
 	npx nx run store:serve 
 	
-	
-	
-pour changer le port de mon app react, je vais dans le fichier nxegghead->apps->store->project.json  et je trouve la clé targets.serve.options a laquelle je rajoute dans les options un "port":4201 par example
+========= NOTA BENE		
+	pour changer le port de mon app react, je vais dans le fichier nxegghead->apps->store->project.json  
+	et je trouve la clé targets.serve.options a laquelle je rajoute dans les options un "port":4201 par example
 
 
 
-8) j'ai ensuite crée a la volée un rendu dans nxegghead->apps->store->src->app->app.tsx en typescript pour voir un rendu, en installant d'abord mui, 
+
+7) j'ai ensuite crée a la volée un rendu dans nxegghead->apps->store->src->app->app.tsx en typescript pour voir un rendu, en installant d'abord mui, 
 	et donc j'ai du tapper les commandes suivantes depuis le dossier nxegghead directement:
 		npm install @mui/material @mui/styled-engine-sc
 		npm install @emotion/react 
@@ -120,13 +128,14 @@ pour changer le port de mon app react, je vais dans le fichier nxegghead->apps->
 	et j'ai ensuite crée un dossier app->store->src->fake_api->index.ts dans lequel j'ai cree des données bidons
 	et j'ai ensuite utilisé ces données dans le fichier app->store->src->app->app.tsx    
 
-		
-9) pour lancer un lint de notre projet: npx nx run store:lint
+========= NOTA BENE				
+	pour lancer un lint de notre projet: npx nx run store:lint
 
 
 
-10 ) puis j'ai fait mon premier push sur git, pour cela, j'ai du d'abord creer un repo sur github, que j'ai en loccurrence nommé nx_monorepo, 
-puis j'ai copié l'adresse url de ce repository, à  savoir https://github.com/mihoadiego/nx_monorepo.git (pour la 4eme commande ci dessous)
+8 ) premier push sur git, 
+		pour cela, j'ai du d'abord creer un repo sur github, que j'ai en loccurrence nommé nx_monorepo, 
+		puis j'ai copié l'adresse url de ce repo, à  savoir https://github.com/mihoadiego/nx_monorepo.git (pour la 4eme commande ci dessous)
 		puis je suis retourné sur mon projet, et directement à la source de nxegghead, j'ai tappé:
 			git init
 			git add .
@@ -134,17 +143,26 @@ puis j'ai copié l'adresse url de ce repository, à  savoir https://github.com/m
 			git remote add origin https://github.com/mihoadiego/nx_monorepo.git
 			git push -u -f origin main
 
-			et ca m'a permis, apres connection depuis VS code au github, de push mon monorepo!
+		et ca m'a permis, apres connection depuis VS code au github, de push mon monorepo!
+
+jen suis a l'etape 7 du tuto
 
 
+9) gestion des images
+	le dossier nxegghead->apps->store->src->assets permet de stocker toutes les images que l'on veut.
+	elles sont ensuite accessibles dans notre appli react, via par exemple  image: '/assets/larson.jpg'
 
-
-
-
-
-
-jen suis a l'etape 6 du tuto
-
-
+========= NOTA BENE				
+    les dossiers accessibles pour les assets sont dans nxegghead->apps->store->project.json
+	précisément, dans ce fichier json, dans la clé targets.build.assets
+	on peut d'ailleurs paramétrer ces assets comme ci dessous:
+				          "assets": ["apps/store/src/favicon.ico", 
+						  			 "apps/store/src/assets", 
+									 {
+										"input":"./example",          -> la ou va pointer une adresse de dossier par ex , en locurrnce ici un dossier qui serait dans nxegghead->apps->store et qui s'appelerait example  
+										"glob": "*.png",			  -> pour lui dire de prendre tous les fichiers png du dossier précisé dans input
+										"output":"./xxxxx" 		      -> adresse pointant sur l'endroit ou les assets devraient etre copiées au moment du compile (dist)
+									  }
+									],
 
 */
